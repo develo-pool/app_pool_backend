32e5ec6bbe988e2ba20b1c5c9f8465b34120d7ca
Lee-DoHa <johnny550@naver.com>
2022-07-12 14:54:38 +0900

    Create Message Service

--- src/main/java/app/pool/project/service/MessageService.java
+++ src/main/java/app/pool/project/service/MessageService.java
@@ -1,4 +1,33 @@
 package app.pool.project.service;
 
+import app.pool.project.entity.Message;
+import app.pool.project.repository.MessageRepository;
+import app.pool.project.request.MessageCreate;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
 public class MessageService {
+
+    private final MessageRepository messageRepository;
+
+//    @Value("${messageImg.path}")
+    private String uploadFolder;
+
+
+    public void write(MessageCreate messageCreate) {
+
+        Message message = Message.builder()
+                .title(messageCreate.getTitle())
+                .body(messageCreate.getBody())
+                .messageLink(messageCreate.getMessageLink())
+                .build();
+    }
+
+
 }
