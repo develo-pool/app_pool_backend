1e4833cef025978f33baa63095b883a10aba6f24
Lee-DoHa <johnny550@naver.com>
2022-07-13 16:07:51 +0900

    Update MessageRepositoryCustom.java

--- src/main/java/app/pool/project/repository/MessageRepositoryCustom.java
+++ src/main/java/app/pool/project/repository/MessageRepositoryCustom.java
@@ -1,6 +1,6 @@
 package app.pool.project.repository;
 
-import app.pool.project.entity.Message;
+import app.pool.project.domain.Message;
 import app.pool.project.request.MessageSearch;
 
 import java.util.List;
