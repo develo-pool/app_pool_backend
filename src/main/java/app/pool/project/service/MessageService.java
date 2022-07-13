30688028ad650c2e91b0c5ec8dc6f94f43b8cb22
Lee-DoHa <johnny550@naver.com>
2022-07-13 16:08:18 +0900

    Update MessageService.java

--- src/main/java/app/pool/project/service/MessageService.java
+++ src/main/java/app/pool/project/service/MessageService.java
@@ -1,13 +1,23 @@
 package app.pool.project.service;
 
-import app.pool.project.entity.Message;
+import app.pool.project.domain.Message;
+import app.pool.project.domain.MessageEditor;
+import app.pool.project.domain.Photo;
+import app.pool.project.exception.MessageNotFound;
 import app.pool.project.repository.MessageRepository;
+import app.pool.project.repository.PhotoRepository;
 import app.pool.project.request.MessageCreate;
+import app.pool.project.request.MessageEdit;
+import app.pool.project.request.MessageSearch;
+import app.pool.project.response.MessageResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -15,8 +25,10 @@ import org.springframework.transaction.annotation.Transactional;
 public class MessageService {
 
     private final MessageRepository messageRepository;
+    private final PhotoRepository photoRepository;
+    private final FileHandler fileHandler;
 
-    public void write(MessageCreate messageCreate) {
+    public void write(MessageCreate messageCreate, List<MultipartFile> files) throws Exception{
 
         Message message = Message.builder()
                 .title(messageCreate.getTitle())
@@ -24,25 +36,63 @@ public class MessageService {
                 .messageLink(messageCreate.getMessageLink())
                 .build();
 
+        List<Photo> photoList = fileHandler.parseFileInfo(files);
+
+        if (!photoList.isEmpty()) {
+            for (Photo photo : photoList) {
+                message.addPhoto(photoRepository.save(photo));
+            }
+        }
+
         messageRepository.save(message);
     }
 
 
-    public void get() {
+    public MessageResponse get(Long id) {
+        Message message = messageRepository.findById(id)
+                .orElseThrow(MessageNotFound::new);
 
+        return MessageResponse.builder()
+                .id(message.getId())
+                .title(message.getTitle())
+                .body(message.getBody())
+                .messageLink(message.getMessageLink())
+                .build();
     }
 
-    public void getList() {
-
+    public List<MessageResponse> getList(MessageSearch messageSearch) {
+        return messageRepository.getList(messageSearch).stream()
+                .map(MessageResponse::new)
+                .collect(Collectors.toList());
     }
 
     @Transactional
-    public void edit() {
+    public void edit(Long id, MessageEdit messageEdit) {
+        Message message = messageRepository.findById(id)
+                .orElseThrow(MessageNotFound::new);
 
+        MessageEditor.MessageEditorBuilder editorBuilder = message.toEditor();
+
+        if (messageEdit.getTitle() != null) {
+            editorBuilder.title(messageEdit.getTitle());
+        }
+
+        if (messageEdit.getBody() != null) {
+            editorBuilder.body(messageEdit.getBody());
+        }
+
+        if (messageEdit.getMessageLink() != null) {
+            editorBuilder.messageLink(messageEdit.getMessageLink());
+        }
+
+        message.edit(editorBuilder.build());
     }
 
-    public void delete() {
+    public void delete(Long id) {
+        Message message = messageRepository.findById(id)
+                .orElseThrow(MessageNotFound::new);
 
+        messageRepository.delete(message);
     }
 
 }
