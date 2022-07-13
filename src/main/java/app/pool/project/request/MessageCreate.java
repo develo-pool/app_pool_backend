168df06305700e3fa7e7e19d06fcaad0fbe674e8
Lee-DoHa <johnny550@naver.com>
2022-07-13 16:07:20 +0900

    Update MessageCreate.java

--- src/main/java/app/pool/project/request/MessageCreate.java
+++ src/main/java/app/pool/project/request/MessageCreate.java
@@ -4,6 +4,9 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -14,12 +17,13 @@ public class MessageCreate {
     private String title;
     private String body;
     private String messageLink;
+    private List<MultipartFile> files;
 
     @Builder
-
-    public MessageCreate(String title, String body, String messageLink) {
+    public MessageCreate(String title, String body, String messageLink, List<MultipartFile> files) {
         this.title = title;
         this.body = body;
         this.messageLink = messageLink;
+        this.files = files;
     }
 }
