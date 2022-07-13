a4c0ef65cf08ba117c9b3094625da5f5738d9a3b
Lee-DoHa <johnny550@naver.com>
2022-07-13 16:07:43 +0900

    Update MessageRepository.java

--- src/main/java/app/pool/project/repository/MessageRepository.java
+++ src/main/java/app/pool/project/repository/MessageRepository.java
@@ -1,9 +1,7 @@
 package app.pool.project.repository;
 
-import app.pool.project.entity.Message;
+import app.pool.project.domain.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 
 public interface MessageRepository extends JpaRepository<Message, Long>, MessageRepositoryCustom {
 //    Message save(Message message);
