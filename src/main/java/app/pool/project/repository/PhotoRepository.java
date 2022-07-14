da5548a2b1f8fce9996f2b04e34ea5535ddd6ff7
Lee-DoHa <johnny550@naver.com>
2022-07-14 15:37:11 +0900

    이미지 기능 삭제

--- src/main/java/app/pool/project/repository/PhotoRepository.java
+++
@@ -1,7 +0,0 @@
-package app.pool.project.repository;
-
-import app.pool.project.domain.Photo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PhotoRepository extends JpaRepository<Photo, Long> {
-}
--- src/main/java/app/pool/project/service/PhotoService.java
+++
@@ -1,7 +0,0 @@
-package app.pool.project.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class PhotoService {
-}
