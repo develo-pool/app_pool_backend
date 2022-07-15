e482f35ce4115bfd23223cec2e080947d7e444e2
Lee-DoHa <johnny550@naver.com>
2022-07-15 16:00:33 +0900

    회원 Service 구현

---
+++ src/main/java/app/pool/project/service/UserService.java
@@ -0,0 +1,13 @@
+package app.pool.project.service;
+
+import app.pool.project.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+}
