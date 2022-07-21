82ec6c16f50a4f9c21aacf188d73b48bdf7d7f84
Lee-DoHa <johnny550@naver.com>
2022-07-21 11:10:35 +0900

    UserException 생성

--- src/main/java/app/pool/project/exception/PoolUserException.java
+++ src/main/java/app/pool/project/exception/PoolUserException.java
@@ -1,4 +1,21 @@
 package app.pool.project.exception;
 
-public class PoolUserException {
+public class PoolUserException extends BaseException {
+
+    private BaseExceptionType exceptionType;
+
+    public PoolUserException(BaseExceptionType exceptionType) {
+        super();
+        this.exceptionType = exceptionType;
+    }
+
+    @Override
+    public int getStatusCode() {
+        return 404;
+    }
+
+    @Override
+    public BaseExceptionType getExceptionType() {
+        return exceptionType;
+    }
 }
