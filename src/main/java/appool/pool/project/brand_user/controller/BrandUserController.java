89eec68b7e811b3030304c0bbeef34316b231180
Lee-DoHa <johnny550@naver.com>
2022-08-03 00:28:28 +0900

    BrandUser API완성

---
+++ src/main/java/appool/pool/project/brand_user/controller/BrandUserController.java
@@ -0,0 +1,42 @@
+package appool.pool.project.brand_user.controller;
+
+import appool.pool.project.brand_user.dto.BrandUserCreateDto;
+import appool.pool.project.brand_user.dto.BrandUserInfoDto;
+import appool.pool.project.brand_user.service.BrandUserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+public class BrandUserController {
+
+    private final BrandUserService brandUserService;
+
+    @PostMapping("/brand/create")
+    @ResponseStatus(HttpStatus.OK)
+    public void submit(@Valid @RequestBody BrandUserCreateDto brandUserCreateDto) {
+        brandUserService.submit(brandUserCreateDto);
+    }
+
+    @GetMapping("/brand/{id}")
+    public ResponseEntity getBrandInfo(@Valid @PathVariable("id") Long id) {
+        BrandUserInfoDto info = brandUserService.getBrandInfo(id);
+        return new ResponseEntity(info, HttpStatus.OK);
+    }
+
+    @GetMapping("/brand")
+    public ResponseEntity getMyBrandInfo(HttpServletResponse response) {
+        BrandUserInfoDto info = brandUserService.getMyBrandInfo();
+        return new ResponseEntity(info, HttpStatus.OK);
+    }
+
+    @GetMapping("/brand-brandUsernames/{brandUsername}/exists")
+    public ResponseEntity<Boolean> checkBrandUsernameDuplicate(@PathVariable String brandUsername) {
+        return ResponseEntity.ok(brandUserService.checkBrandUsernameDuplicate(brandUsername));
+    }
+}
