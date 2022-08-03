fafd5657fbb998d925db47cb057ca9532dd9bffb
Lee-DoHa <johnny550@naver.com>
2022-08-03 17:56:59 +0900

    Update MessageCreate.java

--- src/main/java/appool/pool/project/message/dto/MessageCreate.java
+++ src/main/java/appool/pool/project/message/dto/MessageCreate.java
@@ -1,10 +1,8 @@
 package appool.pool.project.message.dto;
 
+import appool.pool.project.file.service.AWSS3UploadService;
 import appool.pool.project.message.Message;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.constraints.NotBlank;
@@ -16,14 +14,17 @@ import java.util.Optional;
 public record MessageCreate(@NotBlank(message = "제목을 입력해주세요") String title,
                             @NotBlank(message = "내용을 입력해주세요") String body,
                             String messageLink,
-                            List<String> filePath) {
+                            List<MultipartFile> filePath) {
+
+    public static AWSS3UploadService awss3UploadService;
 
     public Message toEntity() {
+        List<String> uploadImage = awss3UploadService.uploadImage(filePath);
         return Message.builder()
                 .title(title)
                 .body(body)
                 .messageLink(messageLink)
-                .filePath(filePath)
+                .filePath(uploadImage)
                 .build();
     }
 }
