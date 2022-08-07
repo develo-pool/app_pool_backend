e41e64cd6db6a3c8e8e5f261dc06cf639f3a7442
Lee-DoHa <johnny550@naver.com>
2022-08-07 16:36:24 +0900

    AWS S3 Transactional 추가

--- src/main/java/appool/pool/project/message/dto/MessageCreate.java
+++ src/main/java/appool/pool/project/message/dto/MessageCreate.java
@@ -3,13 +3,13 @@ package appool.pool.project.message.dto;
 import appool.pool.project.file.service.AWSS3UploadService;
 import appool.pool.project.message.Message;
 import lombok.*;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.constraints.NotBlank;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-
 
 public record MessageCreate(@NotBlank(message = "제목을 입력해주세요") String title,
                             @NotBlank(message = "내용을 입력해주세요") String body,
