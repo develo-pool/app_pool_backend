f9c159de1d7a907f01a2e093fd54d86ecfc1ece6
Lee-DoHa <johnny550@naver.com>
2022-08-08 11:04:00 +0900

    Update MessageCreate Dto

--- src/main/java/appool/pool/project/message/dto/MessageCreate.java
+++ src/main/java/appool/pool/project/message/dto/MessageCreate.java
@@ -1,6 +1,5 @@
 package appool.pool.project.message.dto;
 
-import appool.pool.project.file.service.AWSS3UploadService;
 import appool.pool.project.message.Message;
 import lombok.*;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,18 +12,14 @@ import java.util.Optional;
 
 public record MessageCreate(@NotBlank(message = "제목을 입력해주세요") String title,
                             @NotBlank(message = "내용을 입력해주세요") String body,
-                            String messageLink,
-                            List<MultipartFile> filePath) {
+                            String messageLink) {
 
-    public static AWSS3UploadService awss3UploadService;
 
     public Message toEntity() {
-        List<String> uploadImage = awss3UploadService.uploadImage(filePath);
         return Message.builder()
                 .title(title)
                 .body(body)
                 .messageLink(messageLink)
-                .filePath(uploadImage)
                 .build();
     }
 }
