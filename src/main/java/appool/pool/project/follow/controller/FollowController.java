5c5c953f91f99a3c0d60064d75f1ba38f4076d79
Lee-DoHa <johnny550@naver.com>
2022-08-08 17:33:30 +0900

    Follow 로직 변경

--- src/main/java/appool/pool/project/follow/controller/FollowController.java
+++ src/main/java/appool/pool/project/follow/controller/FollowController.java
@@ -1,6 +1,8 @@
 package appool.pool.project.follow.controller;
 
 import appool.pool.project.follow.service.FollowService;
+import appool.pool.project.user.PoolUser;
+import appool.pool.project.user.repository.UserRepository;
 import appool.pool.project.util.security.SecurityUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -10,21 +12,26 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RequiredArgsConstructor
 @RestController
 public class FollowController {
 
     private final FollowService followService;
+    private final UserRepository userRepository;
 
     @PostMapping("/follow/{toUserId}")
     public ResponseEntity<?> followUser(@PathVariable long toUserId) {
-        followService.follow(SecurityUtil.getLoginUserId(), toUserId);
+        Optional<PoolUser> poolUserMy = userRepository.findByUsername(SecurityUtil.getLoginUsername());
+        followService.follow(poolUserMy.get().getId(), toUserId);
         return new ResponseEntity<>("팔로우 성공", HttpStatus.OK);
     }
 
     @DeleteMapping("/follow/{toUserId}")
     public ResponseEntity<?> unFollowUser(@PathVariable long toUserId) {
-        followService.unFollow(SecurityUtil.getLoginUserId(), toUserId);
+        Optional<PoolUser> poolUserMy = userRepository.findByUsername(SecurityUtil.getLoginUsername());
+        followService.unFollow(poolUserMy.get().getId(), toUserId);
         return new ResponseEntity<>("팔로우 취소 성공", HttpStatus.OK);
     }
 }
--- src/main/java/appool/pool/project/util/security/SecurityUtil.java
+++ src/main/java/appool/pool/project/util/security/SecurityUtil.java
@@ -10,19 +10,9 @@ import java.util.Optional;
 
 @RequiredArgsConstructor
 public class SecurityUtil {
-    static UserRepository userRepository;
     public static String getLoginUsername() {
         UserDetails user = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
         return user.getUsername();
-    }
-
-    public static Long getLoginUserId() {
-        UserDetails user = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        String username = user.getUsername();
-
-        Optional<PoolUser> poolUser = userRepository.findByUsername(username);
-
-        return poolUser.get().getId();
     }
 }
