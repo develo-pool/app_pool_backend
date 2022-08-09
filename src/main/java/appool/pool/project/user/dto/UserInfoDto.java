eaab6f503ecd7b041041faf91f45898ea7bfe86b
Lee-DoHa <johnny550@naver.com>
2022-08-09 16:02:34 +0900

    UserInfoDto 수정, 속성 추가

--- src/main/java/appool/pool/project/user/dto/UserInfoDto.java
+++ src/main/java/appool/pool/project/user/dto/UserInfoDto.java
@@ -2,23 +2,20 @@ package appool.pool.project.user.dto;
 
 import appool.pool.project.user.PoolUser;
 import appool.pool.project.user.UserStatus;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Data
 @NoArgsConstructor
+@Builder
+@Getter
+@AllArgsConstructor
 public class UserInfoDto {
 
     private String username;
     private String nickName;
     private UserStatus userStatus;
+    private boolean follow;
+    private int userFollowerCount;
+    private int userFollowingCount;
 
-
-    @Builder
-    public UserInfoDto(PoolUser poolUser) {
-        this.username = poolUser.getUsername();
-        this.nickName = poolUser.getNickName();
-        this.userStatus = poolUser.getUserStatus();
-    }
 }
