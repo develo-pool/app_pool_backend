5003ce16584a9559d408dbbba111bf79c2d0ec66
Lee-DoHa <johnny550@naver.com>
2022-08-09 17:24:59 +0900

    BrandUser Pagination용 repository Query 추가

--- src/main/java/appool/pool/project/brand_user/repository/BrandUserRepository.java
+++ src/main/java/appool/pool/project/brand_user/repository/BrandUserRepository.java
@@ -1,8 +1,11 @@
 package appool.pool.project.brand_user.repository;
 
 import appool.pool.project.brand_user.BrandUser;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BrandUserRepository extends JpaRepository<BrandUser, Long> {
@@ -10,4 +13,10 @@ public interface BrandUserRepository extends JpaRepository<BrandUser, Long> {
     Optional<BrandUser> findByBrandUsername(String brandUsername);
 
     boolean existsByBrandUsername(String brandUsername);
+
+    @Query(value = "SELECT * FROM brand_user ORDER BY brand_user_id DESC", nativeQuery = true)
+    List<BrandUser> brandList(Pageable pageable);
+
+    @Query(value = "SELECT * FROM brand_user WHERE brand_user_id < :id ORDER BY brand_user_id DESC", nativeQuery = true)
+    List<BrandUser> brandListLess(Long id, Pageable pageable);
 }
