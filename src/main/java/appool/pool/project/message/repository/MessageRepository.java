72891d8cdf49176d3e364268d06a46b83f82d0f1
Lee-DoHa <johnny550@naver.com>
2022-08-09 11:40:52 +0900

    Follow 유저의 게시물만 필터링

--- src/main/java/appool/pool/project/message/repository/MessageRepository.java
+++ src/main/java/appool/pool/project/message/repository/MessageRepository.java
@@ -1,9 +1,13 @@
 package appool.pool.project.message.repository;
 
 import appool.pool.project.message.Message;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MessageRepository extends JpaRepository<Message, Long>, MessageRepositoryCustom {
@@ -11,4 +15,9 @@ public interface MessageRepository extends JpaRepository<Message, Long>, Message
     @EntityGraph(attributePaths = {"writer"})
     Optional<Message> findWithWriterById(Long id);
 
+    @Query(value = "SELECT * FROM message WHERE writer_id IN (SELECT to_user_id FROM follow WHERE from_user_id = :sessionId) ORDER BY message_id DESC", nativeQuery = true)
+    List<Message> mainFeed(long sessionId, Pageable pageable);
+
+    @Query(value = "SELECT * FROM message WHERE writer_id IN (SELECT to_user_id FROM follow WHERE from_user_id = :sessionId) ORDER BY (message_id < a = :id) DESC", nativeQuery = true)
+    List<Message> mainFeedLess(long sessionId, Long id, Pageable pageable);
 }
