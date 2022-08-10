1642183af452a632ba9519361f5f630c639666cf
Lee-DoHa <johnny550@naver.com>
2022-08-10 16:51:19 +0900

    Comment 생성, 조회 기능 구현

--- src/main/java/appool/pool/project/comment/service/CommentService.java
+++ src/main/java/appool/pool/project/comment/service/CommentService.java
@@ -1,51 +1,57 @@
 package appool.pool.project.comment.service;
 
 import appool.pool.project.comment.Comment;
+import appool.pool.project.comment.dto.CommentCreate;
 import appool.pool.project.comment.dto.CommentResponse;
 import appool.pool.project.message.Message;
 import appool.pool.project.comment.repository.CommentRepository;
 import appool.pool.project.message.repository.MessageRepository;
+import appool.pool.project.user.PoolUser;
+import appool.pool.project.user.repository.UserRepository;
+import appool.pool.project.util.security.SecurityUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
-
 @Service
+@Transactional
 public class CommentService {
 
-    private final CommentRepository commentRepository;
+    private final UserRepository userRepository;
     private final MessageRepository messageRepository;
+    private final CommentRepository commentRepository;
 
-    @Transactional
-    public void addComment(String body, long messageId) {
-        Message message = messageRepository.findById(messageId).get();
-        Comment comment = Comment.builder()
-                .body(body)
-                .message(message)
-                .build();
+    public void create(CommentCreate commentCreate, Long messageId) {
+        Comment comment = commentCreate.toEntity();
+        comment.confirmWriter(userRepository.findByUsername(SecurityUtil.getLoginUsername()).get());
+        comment.confirmMessage(messageRepository.findById(messageId).get());
+
         commentRepository.save(comment);
     }
 
-    @Transactional
-    public List<CommentResponse> getList(CommentSearch commentSearch) {
-        return commentRepository.getList(commentSearch).stream()
+    public List<CommentResponse> getList(Long messageId, Long cursor, Pageable pageable) {
+        return getCommentList(messageId, cursor, pageable).stream()
                 .map(CommentResponse::new)
                 .collect(Collectors.toList());
     }
 
-//    @Transactional
-//    public void deleteComment(long id){
-//        Comment comment= commentRepository.findById(id);
-//        commentRepository.delete(comment);
-//    }
+    private List<Comment> getCommentList(Long messageId, Long id, Pageable pageable) {
+        return id.equals(0L)
+                ? commentRepository.commentList(messageId, pageable)
+                : commentRepository.commentListLess(messageId, id, pageable);
+    }
 
-//    public CommentResponse get(Long id) {
-//        commentRepository.(id)
-//    }
-
+    public CommentResponse getMyComment(Long messageId) {
+        Optional<PoolUser> loginUser = userRepository.findByUsername(SecurityUtil.getLoginUsername());
+        Comment myComment = commentRepository.findCommentByMessageIdAndWriterId(messageId, loginUser.get().getId());
+        CommentResponse commentResponse = new CommentResponse(myComment);
+        return commentResponse;
+    }
 
 }
