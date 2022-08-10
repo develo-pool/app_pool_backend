7e99220821bc802e25cb406c663747b8384ed283
Lee-DoHa <johnny550@naver.com>
2022-08-10 16:51:51 +0900

    Comment 관련 query 추가

--- src/main/java/appool/pool/project/comment/repository/CommentRepository.java
+++ src/main/java/appool/pool/project/comment/repository/CommentRepository.java
@@ -1,10 +1,19 @@
 package appool.pool.project.comment.repository;
 
 import appool.pool.project.comment.Comment;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface CommentRepository extends JpaRepository<Comment, Long>, CommentRepositoryCustom{
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Long>{
 
     Comment findCommentByMessageIdAndWriterId(long message_id, long writer_id);
 
+    @Query(value = "SELECT * FROM comment WHERE message_id = :messageId ORDER BY comment_id DESC", nativeQuery = true)
+    List<Comment> commentList(long messageId, Pageable pageable);
+
+    @Query(value = "SELECT * FROM comment WHERE message_id = :messageId AND comment_id < :id ORDER BY comment_id DESC", nativeQuery = true)
+    List<Comment> commentListLess(long messageId, Long id, Pageable pageable);
 }
