b6b43359deabeb20479b10a32b1664e4b88219ce
Lee-DoHa <johnny550@naver.com>
2022-08-10 14:43:32 +0900

    댓글 작성여부 체크용 query 추가

--- src/main/java/appool/pool/project/comment/repository/CommentRepository.java
+++ src/main/java/appool/pool/project/comment/repository/CommentRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long>, CommentRepositoryCustom{
 
+    Comment findCommentByMessageIdAndWriterId(long message_id, long writer_id);
+
 }
