51f30adb3768c0ef95e46e2f890de3afb90a84de
Lee-DoHa <johnny550@naver.com>
2022-08-11 15:34:05 +0900

    FCM Entity 생성

---
+++ src/main/java/appool/pool/project/fcm/FcmMessage.java
@@ -0,0 +1,30 @@
+package appool.pool.project.fcm;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@AllArgsConstructor
+@Data
+public class FcmMessage {
+    private boolean validate_only;
+    private MessageForm messageForm;
+
+    @Data
+    @AllArgsConstructor
+    @Builder
+    public static class MessageForm {
+        private Notification notification;
+        private String token;
+    }
+
+    @Builder
+    @AllArgsConstructor
+    @Data
+    public static class Notification {
+        private String title;
+        private String body;
+        private String image;
+    }
+}
