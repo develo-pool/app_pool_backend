45d025389775e9b985d1c9c589b4ce103f2d23a0
Lee-DoHa <johnny550@naver.com>
2022-08-15 17:55:52 +0900

    FCM 전송 기능 구현

--- src/main/java/appool/pool/project/fcm/controller/PushNotificationController.java
+++ src/main/java/appool/pool/project/fcm/controller/PushNotificationController.java
@@ -1,9 +1,16 @@
 package appool.pool.project.fcm.controller;
 
+import appool.pool.project.fcm.RequestDTO;
+import appool.pool.project.fcm.service.FirebaseCloudMessageService;
 import appool.pool.project.fcm.service.PushNotificationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @Slf4j
 @RestController
@@ -11,6 +18,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class PushNotificationController {
 
     private final PushNotificationService pushNotificationService;
+
+    private final FirebaseCloudMessageService firebaseCloudMessageService;
+
+    @PostMapping("/api/fcm")
+    public ResponseEntity pushMessage(@RequestBody RequestDTO requestDTO) throws IOException {
+        System.out.println(requestDTO.getTargetToken() + " "
+                +requestDTO.getTitle() + " " + requestDTO.getBody());
+
+        firebaseCloudMessageService.sendMessageTo(
+                requestDTO.getTargetToken(),
+                requestDTO.getTitle(),
+                requestDTO.getBody(),
+                requestDTO.getImage());
+        return ResponseEntity.ok().build();
+    }
 
 
 
