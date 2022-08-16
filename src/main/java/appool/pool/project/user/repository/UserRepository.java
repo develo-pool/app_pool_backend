b100787bab71f41dbe189bc55a76c4fd8f9608eb
Lee-DoHa <johnny550@naver.com>
2022-08-16 16:04:03 +0900

    회원 fcmToken 조회 쿼리 추가

--- src/main/java/appool/pool/project/user/repository/UserRepository.java
+++ src/main/java/appool/pool/project/user/repository/UserRepository.java
@@ -2,7 +2,9 @@ package appool.pool.project.user.repository;
 
 import appool.pool.project.user.PoolUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<PoolUser, Long> {
@@ -16,5 +18,8 @@ public interface UserRepository extends JpaRepository<PoolUser, Long> {
     boolean existsByPhoneNumber(String phoneNumber);
 
     Optional<PoolUser> findByRefreshToken(String refreshToken);
+
+    @Query(value = "SELECT fcm_token FROM pooluser WHERE pool_user_id IN (SELECT from_user_id FROM follow WHERE to_user_id = :sessionId) ORDER BY pool_user_id DESC", nativeQuery = true)
+    List<String> findFcmTokenList(Long sessionId);
 }
  
