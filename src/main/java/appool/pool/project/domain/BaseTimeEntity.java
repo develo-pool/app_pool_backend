bfdc899f75986c3aafa97ee2796db311aa3916e9
Lee-DoHa <johnny550@naver.com>
2022-08-23 18:09:14 +0900

    서버시간 한국시간으로 변경

--- src/main/java/appool/pool/project/domain/BaseTimeEntity.java
+++ src/main/java/appool/pool/project/domain/BaseTimeEntity.java
@@ -17,9 +17,9 @@ public abstract class BaseTimeEntity {
 
     @CreatedDate
     @Column(updatable = false)
-    private LocalDateTime createDate;
+    private LocalDateTime createDate = DateTimeUtils.now();
 
     @LastModifiedDate
     @Column(updatable = true)
-    private LocalDateTime lastModifiedDate;
+    private LocalDateTime lastModifiedDate = DateTimeUtils.now();
 }
---
+++ src/main/java/appool/pool/project/domain/DateTimeUtils.java
@@ -0,0 +1,12 @@
+package appool.pool.project.domain;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+public class DateTimeUtils {
+
+    public static LocalDateTime now() {
+        return ZonedDateTime.now(ZoneId.of("Asia/Seoul")).toLocalDateTime();
+    }
+}
