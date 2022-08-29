bdc485dfa46480e63e5f7ddab3239cb633fa6b17
Lee-DoHa <johnny550@naver.com>
2022-08-29 13:29:27 +0900

    MessageAPI 예외처리 수정, 적용

--- src/main/java/appool/pool/project/message/service/MessageService.java
+++ src/main/java/appool/pool/project/message/service/MessageService.java
@@ -1,5 +1,7 @@
 package appool.pool.project.message.service;
 
+import appool.pool.project.brand_user.exception.BrandUserException;
+import appool.pool.project.brand_user.exception.BrandUserExceptionType;
 import appool.pool.project.brand_user.repository.BrandUserRepository;
 import appool.pool.project.comment.repository.CommentRepository;
 import appool.pool.project.file.exception.FileException;
@@ -11,7 +13,6 @@ import appool.pool.project.user.PoolUser;
 import appool.pool.project.user.exception.PoolUserException;
 import appool.pool.project.user.exception.PoolUserExceptionType;
 import appool.pool.project.message.Message;
-import appool.pool.project.message.dto.MessageEdit;
 import appool.pool.project.message.repository.MessageRepository;
 import appool.pool.project.message.dto.MessageCreate;
 import appool.pool.project.message.dto.MessageResponse;
@@ -20,6 +21,7 @@ import appool.pool.project.util.security.SecurityUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -56,11 +58,19 @@ public class MessageService {
     }
 
     public MessageResponse get(Long id) {
-        Optional<PoolUser> poolUser = userRepository.findByUsername(SecurityUtil.getLoginUsername());
-        MessageResponse messageResponse = new MessageResponse(messageRepository.findWithWriterById(id).orElseThrow(() -> new MessageException(MessageExceptionType.MESSAGE_NOT_FOUND)));
-        messageResponse.setCommentAble(commentRepository.findCommentByMessageIdAndWriterId(messageResponse.getPostId(), poolUser.get().getId()) == null);
-        messageResponse.getWriterDto().getBrandUserInfoDto().setBrandUsername(brandUserRepository.findByPoolUserId(messageResponse.getWriterDto().getPoolUserId()).get().getBrandUsername());
-        messageResponse.getWriterDto().getBrandUserInfoDto().setBrandProfileImage(brandUserRepository.findByPoolUserId(messageResponse.getWriterDto().getPoolUserId()).get().getBrandProfileImage());
+        PoolUser poolUser = userRepository.findByUsername(SecurityUtil.getLoginUsername())
+                .orElseThrow(() -> new PoolUserException(PoolUserExceptionType.NOT_FOUND_MEMBER));
+        MessageResponse messageResponse = new MessageResponse(messageRepository.findWithWriterById(id)
+                .orElseThrow(() -> new MessageException(MessageExceptionType.MESSAGE_NOT_FOUND)));
+
+        messageResponse.setCommentAble(commentRepository.findCommentByMessageIdAndWriterId(messageResponse.getPostId(), poolUser.getId()) == null);
+
+        messageResponse.getWriterDto().getBrandUserInfoDto().setBrandUsername(brandUserRepository.findByPoolUserId(messageResponse.getWriterDto().getPoolUserId())
+                .orElseThrow(() -> new BrandUserException(BrandUserExceptionType.NOT_FOUND_BRAND)).getBrandUsername());
+
+        messageResponse.getWriterDto().getBrandUserInfoDto().setBrandProfileImage(brandUserRepository.findByPoolUserId(messageResponse.getWriterDto().getPoolUserId())
+                .orElseThrow(() -> new BrandUserException(BrandUserExceptionType.NOT_FOUND_BRAND)).getBrandProfileImage());
+
         return messageResponse;
     }
 
@@ -69,21 +79,24 @@ public class MessageService {
      */
 
     public List<MessageResponse> getMainFeed(Long cursor, Pageable pageable) {
-        Optional<PoolUser> poolUser = userRepository.findByUsername(SecurityUtil.getLoginUsername());
+        PoolUser poolUser = userRepository.findByUsername(SecurityUtil.getLoginUsername()).orElseThrow(() -> new PoolUserException(PoolUserExceptionType.NOT_FOUND_MEMBER));
         List<MessageResponse> mainList = getMessageList(cursor, pageable).stream()
                 .map(MessageResponse::new)
                 .collect(Collectors.toList());
 
         mainList.forEach(f -> {
-            f.getWriterDto().getBrandUserInfoDto().setBrandUserId(brandUserRepository.findByPoolUserId(f.getWriterDto().getPoolUserId()).get().getId());
-            f.setCommentAble(commentRepository.findCommentByMessageIdAndWriterId(f.getPostId(), poolUser.get().getId()) == null);
-            f.getWriterDto().getBrandUserInfoDto().setBrandUsername(brandUserRepository.findByPoolUserId(f.getWriterDto().getPoolUserId()).get().getBrandUsername());
-            f.getWriterDto().getBrandUserInfoDto().setBrandProfileImage(brandUserRepository.findByPoolUserId(f.getWriterDto().getPoolUserId()).get().getBrandProfileImage());
+            f.getWriterDto().getBrandUserInfoDto().setBrandUserId(brandUserRepository.findByPoolUserId(f.getWriterDto().getPoolUserId())
+                    .orElseThrow(() -> new BrandUserException(BrandUserExceptionType.NOT_FOUND_BRAND)).getId());
+            f.setCommentAble(commentRepository.findCommentByMessageIdAndWriterId(f.getPostId(), poolUser.getId()) == null);
+            f.getWriterDto().getBrandUserInfoDto().setBrandUsername(brandUserRepository.findByPoolUserId(f.getWriterDto().getPoolUserId())
+                    .orElseThrow(() -> new BrandUserException(BrandUserExceptionType.NOT_FOUND_BRAND)).getBrandUsername());
+            f.getWriterDto().getBrandUserInfoDto().setBrandProfileImage(brandUserRepository.findByPoolUserId(f.getWriterDto().getPoolUserId())
+                    .orElseThrow(() -> new BrandUserException(BrandUserExceptionType.NOT_FOUND_BRAND)).getBrandProfileImage());
         });
 
         List<MessageResponse> result = new ArrayList<>();
         mainList.forEach(f -> {
-            if(f.getCreate_date().isAfter(followRepository.followTime(poolUser.get().getId(), f.getWriterDto().getPoolUserId()))) {
+            if(f.getCreate_date().isAfter(followRepository.followTime(poolUser.getId(), f.getWriterDto().getPoolUserId()))) {
                 result.add(f);
             };
         });
@@ -91,10 +104,11 @@ public class MessageService {
     }
 
     private List<Message> getMessageList(Long id, Pageable page) {
-        Optional<PoolUser> poolUser = userRepository.findByUsername(SecurityUtil.getLoginUsername());
+        PoolUser poolUser = userRepository.findByUsername(SecurityUtil.getLoginUsername())
+                .orElseThrow(() -> new PoolUserException(PoolUserExceptionType.NOT_FOUND_MEMBER));
         return id.equals(0L)
-                ? messageRepository.mainFeed(poolUser.get().getId(), page)
-                : messageRepository.mainFeedLess(poolUser.get().getId(), id, page);
+                ? messageRepository.mainFeed(poolUser.getId(), page)
+                : messageRepository.mainFeedLess(poolUser.getId(), id, page);
     }
 
     /**
@@ -103,25 +117,28 @@ public class MessageService {
      */
 
     public List<MessageResponse> getProfileFeed(Long userId, Long cursor, Pageable pageable) {
-        Optional<PoolUser> poolUser = userRepository.findByUsername(SecurityUtil.getLoginUsername());
+        PoolUser poolUser = userRepository.findByUsername(SecurityUtil.getLoginUsername())
+                .orElseThrow(() -> new PoolUserException(PoolUserExceptionType.NOT_FOUND_MEMBER));
         List<MessageResponse> mainList = getProfileList(userId, cursor, pageable).stream()
                 .map(MessageResponse::new)
                 .collect(Collectors.toList());
 
         mainList.forEach(f -> {
-            f.setCommentAble(commentRepository.findCommentByMessageIdAndWriterId(f.getPostId(), poolUser.get().getId()) == null);
-            if(userId == poolUser.get().getId()) {
+            f.setCommentAble(commentRepository.findCommentByMessageIdAndWriterId(f.getPostId(), poolUser.getId()) == null);
+            if(userId == poolUser.getId()) {
                 f.setIsWriter(true);
             }
-            f.getWriterDto().getBrandUserInfoDto().setBrandUserId(brandUserRepository.findByPoolUserId(f.getWriterDto().getPoolUserId()).get().getId());
-            f.getWriterDto().getBrandUserInfoDto().setBrandUsername(brandUserRepository.findByPoolUserId(f.getWriterDto().getPoolUserId()).get().getBrandUsername());
-            f.getWriterDto().getBrandUserInfoDto().setBrandProfileImage(brandUserRepository.findByPoolUserId(f.getWriterDto().getPoolUserId()).get().getBrandProfileImage());
+            f.getWriterDto().getBrandUserInfoDto().setBrandUserId(brandUserRepository.findByPoolUserId(f.getWriterDto().getPoolUserId())
+                    .orElseThrow(() -> new BrandUserException(BrandUserExceptionType.NOT_FOUND_BRAND)).getId());
+            f.getWriterDto().getBrandUserInfoDto().setBrandUsername(brandUserRepository.findByPoolUserId(f.getWriterDto().getPoolUserId())
+                    .orElseThrow(() -> new BrandUserException(BrandUserExceptionType.NOT_FOUND_BRAND)).getBrandUsername());
+            f.getWriterDto().getBrandUserInfoDto().setBrandProfileImage(brandUserRepository.findByPoolUserId(f.getWriterDto().getPoolUserId())
+                    .orElseThrow(() -> new BrandUserException(BrandUserExceptionType.NOT_FOUND_BRAND)).getBrandProfileImage());
         });
         return mainList;
     }
 
     private List<Message> getProfileList(Long userId, Long id, Pageable page) {
-
         return id.equals(0L)
                 ? messageRepository.profileFeed(userId, page)
                 : messageRepository.profileFeedLess(userId, id, page);
@@ -134,18 +151,23 @@ public class MessageService {
 
         mainList.forEach(f -> {
             f.setCommentAble(false);
-            f.getWriterDto().getBrandUserInfoDto().setBrandUserId(brandUserRepository.findByPoolUserId(f.getWriterDto().getPoolUserId()).get().getId());
-            f.getWriterDto().getBrandUserInfoDto().setBrandUsername(brandUserRepository.findByPoolUserId(f.getWriterDto().getPoolUserId()).get().getBrandUsername());
-            f.getWriterDto().getBrandUserInfoDto().setBrandProfileImage(brandUserRepository.findByPoolUserId(f.getWriterDto().getPoolUserId()).get().getBrandProfileImage());
-            f.getWriterDto().getBrandUserInfoDto().setBrandInfo(brandUserRepository.findByPoolUserId(f.getWriterDto().getPoolUserId()).get().getBrandInfo());
+            f.getWriterDto().getBrandUserInfoDto().setBrandUserId(brandUserRepository.findByPoolUserId(f.getWriterDto().getPoolUserId())
+                    .orElseThrow(() -> new BrandUserException(BrandUserExceptionType.NOT_FOUND_BRAND)).getId());
+            f.getWriterDto().getBrandUserInfoDto().setBrandUsername(brandUserRepository.findByPoolUserId(f.getWriterDto().getPoolUserId())
+                    .orElseThrow(() -> new BrandUserException(BrandUserExceptionType.NOT_FOUND_BRAND)).getBrandUsername());
+            f.getWriterDto().getBrandUserInfoDto().setBrandProfileImage(brandUserRepository.findByPoolUserId(f.getWriterDto().getPoolUserId())
+                    .orElseThrow(() -> new BrandUserException(BrandUserExceptionType.NOT_FOUND_BRAND)).getBrandProfileImage());
+            f.getWriterDto().getBrandUserInfoDto().setBrandInfo(brandUserRepository.findByPoolUserId(f.getWriterDto().getPoolUserId())
+                    .orElseThrow(() -> new BrandUserException(BrandUserExceptionType.NOT_FOUND_BRAND)).getBrandInfo());
             f.getWriterDto().setUserFollowerCount(followRepository.findFollowerCountById(f.getWriterDto().getPoolUserId()));
         });
         return mainList;
     }
 
     private List<Message> getProfileListWeb(Long brandId, Long id, Pageable page) {
+        Long userId = brandUserRepository.findById(brandId)
+                .orElseThrow(() -> new BrandUserException(BrandUserExceptionType.NOT_FOUND_BRAND)).getPoolUser().getId();
 
-        Long userId = brandUserRepository.findById(brandId).get().getPoolUser().getId();
         return id.equals(0L)
                 ? messageRepository.profileFeed(userId, page)
                 : messageRepository.profileFeedLess(userId, id, page);
@@ -156,25 +178,30 @@ public class MessageService {
      */
 
     public List<MessageResponse> getMyProfileFeed(Long cursor, Pageable pageable) {
-        Optional<PoolUser> poolUser = userRepository.findByUsername(SecurityUtil.getLoginUsername());
+        PoolUser poolUser = userRepository.findByUsername(SecurityUtil.getLoginUsername())
+                .orElseThrow(() -> new PoolUserException(PoolUserExceptionType.NOT_FOUND_MEMBER));
         List<MessageResponse> mainList = getMyProfileList(cursor, pageable).stream()
                 .map(MessageResponse::new)
                 .collect(Collectors.toList());
 
         mainList.forEach(f -> {
             f.setIsWriter(true);
-            f.getWriterDto().getBrandUserInfoDto().setBrandUserId(brandUserRepository.findByPoolUserId(poolUser.get().getId()).get().getId());
-            f.getWriterDto().getBrandUserInfoDto().setBrandUsername(brandUserRepository.findByPoolUserId(poolUser.get().getId()).get().getBrandUsername());
-            f.getWriterDto().getBrandUserInfoDto().setBrandProfileImage(brandUserRepository.findByPoolUserId(poolUser.get().getId()).get().getBrandProfileImage());
+            f.getWriterDto().getBrandUserInfoDto().setBrandUserId(brandUserRepository.findByPoolUserId(poolUser.getId())
+                    .orElseThrow(() -> new BrandUserException(BrandUserExceptionType.NOT_FOUND_BRAND)).getId());
+            f.getWriterDto().getBrandUserInfoDto().setBrandUsername(brandUserRepository.findByPoolUserId(poolUser.getId())
+                    .orElseThrow(() -> new BrandUserException(BrandUserExceptionType.NOT_FOUND_BRAND)).getBrandUsername());
+            f.getWriterDto().getBrandUserInfoDto().setBrandProfileImage(brandUserRepository.findByPoolUserId(poolUser.getId())
+                    .orElseThrow(() -> new BrandUserException(BrandUserExceptionType.NOT_FOUND_BRAND)).getBrandProfileImage());
         });
         return mainList;
     }
 
     private List<Message> getMyProfileList(Long id, Pageable page) {
-        Optional<PoolUser> poolUser = userRepository.findByUsername(SecurityUtil.getLoginUsername());
+        PoolUser poolUser = userRepository.findByUsername(SecurityUtil.getLoginUsername())
+                .orElseThrow(() -> new PoolUserException(PoolUserExceptionType.NOT_FOUND_MEMBER));
         return id.equals(0L)
-                ? messageRepository.myProfileFeed(poolUser.get().getId(), page)
-                : messageRepository.myProfileFeedLess(poolUser.get().getId(), id, page);
+                ? messageRepository.myProfileFeed(poolUser.getId(), page)
+                : messageRepository.myProfileFeedLess(poolUser.getId(), id, page);
     }
 
 
