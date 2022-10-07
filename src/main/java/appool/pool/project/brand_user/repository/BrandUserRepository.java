80b3b502d8f269be977017769fc730b8827bdb1f
Lee-DoHa <johnny550@naver.com>
2022-10-07 14:15:53 +0900

    web용 Brand 추천 쿼리 수정

--- src/main/java/appool/pool/project/brand_user/repository/BrandUserRepository.java
+++ src/main/java/appool/pool/project/brand_user/repository/BrandUserRepository.java
@@ -28,6 +28,6 @@ public interface BrandUserRepository extends JpaRepository<BrandUser, Long> {
     @Query(value = "SELECT * FROM brand_user WHERE pool_user_id IN (SELECT to_user_id FROM follow WHERE from_user_id = :sessionId) AND brand_user_id < :id ORDER BY brand_user_id DESC", nativeQuery = true)
     List<BrandUser> followingListLess(long sessionId, Long id, Pageable pageable);
 
-    @Query(value = "SELECT * FROM brand_user ORDER BY brand_user_id DESC limit 3", nativeQuery = true)
+    @Query(value = "SELECT * FROM brand_user WHERE pool_user_id IN (SELECT pool_user_id FROM pooluser WHERE user_status = 'BRAND_USER')  ORDER BY brand_user_id DESC limit 3", nativeQuery = true)
     List<BrandUser> recommendBrand();
 }
